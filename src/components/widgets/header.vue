88619053d9e6de0487085d9d1f644367d52d8ae4
karthik.selvaraj <karthik.selvaraj@diatoz.com>
2020-11-30 17:54:51 +0530

    header, search bar

--- src/components/widgets/header.vue
+++ src/components/widgets/header.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="header_hydroid">
-    <img class="logo" src="@/assets/Hydroid.jpg" />
-    <button @click="logout()" class="logout">Logout</button>
+    <img class="logo" src="@/assets/menu_logo_shadow.png" />
+    <search_bar class="search_bar"/>
+    <img class="notifications" src="@/assets/bell.svg"/>
+    <profile_button @click="logout()" class="profile_btn"/>
+    <!-- <button @click="logout()" class="logout">Logout</button> -->
   </div>
 </template>
 <script>
+import search_bar from "@/components/widgets/search_bar.vue";
+import profile_button from "@/components/widgets/headerProButton.vue";
 export default {
     data(){
         return{
 
         }
+    },
+    components:{
+      search_bar,
+      profile_button
     },
     methods:{
         logout(){
@@ -21,6 +30,11 @@ export default {
 }
 </script>
 <style scoped>
+.search_bar{
+  position:absolute;
+  right:12rem;
+  margin-top:.4rem;
+}
 .header_hydroid {
   width: 100%;
   overflow-x: hidden;
@@ -34,14 +48,11 @@ export default {
 }
 .logo {
   width: 6rem;
-  height: 2.3rem;
+  height: 2.8rem;
   border-radius: 5px;
   position: absolute;
   top:7px;
   left:2.5rem;
-  -webkit-box-shadow: 2px -4px 17px 0px rgba(199, 197, 199, 1);
-  -moz-box-shadow: 2px -4px 17px 0px rgba(199, 197, 199, 1);
-  box-shadow: 2px -4px 17px 0px rgba(199, 197, 199, 1);
 }
 .logout{
     position:absolute;
@@ -51,5 +62,15 @@ export default {
     background:white;
     border:1px solid black;
     cursor:pointer;
+}
+.notifications{
+  width:1.6rem;
+  position:absolute;
+  top:.7rem;  
+  right:14rem;
+}
+.profile_btn{
+  position:absolute;
+  right:3rem;
 }
 </style>
---
+++ src/components/widgets/headerProButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="pro_button">
+        <img class="person" src="@/assets/person.svg"/>
+        <p class="username">Username</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style scoped>
+.pro_button{
+
+    padding:0rem 1rem;
+    border-radius:5px;
+    display:flex;   
+    align-items:center;
+    height:2.6rem;
+    box-shadow: 0px 3px 3px #00000024;
+border-radius: 12px;
+}
+.person{
+    width:1.8rem;
+    border-radius:50%;
+    box-shadow: 0px 3px 3px #00000029;
+}
+.username{
+    display:inline-block;
+    margin-left:1rem;
+}
+</style>

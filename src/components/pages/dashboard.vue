b882b7931f3b7c3ef30b88bc759ea582b44d58d4
karthik.selvaraj <karthik.selvaraj@diatoz.com>
2020-12-23 00:14:32 +0530

    color cards on the bottom

--- src/components/pages/dashboard.vue
+++ src/components/pages/dashboard.vue
@@ -6,13 +6,17 @@
       </blacktop>
       <blacktop class="notification_bt" :title="'Notification'" ><notificationCol/></blacktop>
     </div>
+    <p class="other_data_heading">Other Data Readings</p>
+    <div class="row_elements">
+       <colorCards v-for="i in 4" :color="i" :key="i"/>
+    </div>
   </div>
 </template>
 <script>
 import barchart from "@/components/charts/bar";
 import blacktop from "@/components/widgets/blacktop.vue";
 import notificationCol from "@/components/widgets/notificationcol.vue";
-
+import colorCards from "@/components/widgets/colorCards.vue";
 export default {
   data() {
     return {};
@@ -20,17 +24,26 @@ export default {
   components: {
     barchart,
     blacktop,
-    notificationCol
+    notificationCol,
+    colorCards
   }
 };
 </script>
 <style scoped>
 .dashboard {
+  height:calc(100vh - 5rem);
+  overflow-y:scroll;
 }
 .graph_bt {
   width: 80%;
   height: 30rem;
   margin-right: 1rem;
+}
+.other_data_heading{
+  font-family: Arial, Helvetica, sans-serif;
+color: #5953B5;
+font-weight:600;
+font-size:19px;
 }
 .notification_bt {
   width: 25%;
@@ -40,5 +53,11 @@ export default {
 }
 .first_row {
   display: flex;
+}
+.row_elements{
+  display:flex;
+  justify-content: space-between;
+  padding-right:15px;
+  /* background:red; */
 }
 </style>
---
+++ src/components/widgets/colorCards.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="colorChoose(color)" class="color-cards">
+    <p class="color_heading">Heading</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ["color"],
+  data() {
+    return {};
+  },
+  methods: {
+    colorChoose(key) {
+      switch (key) {
+        case 1:
+          return "orange";
+        case 2:
+          return "blue";
+        case 3:
+          return "green";
+        default:
+          return "brown";
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.orange {
+  background: transparent linear-gradient(300deg, #f83600 0%, #fe8700 100%) 0%
+    0% no-repeat padding-box;
+}
+.blue {
+  background: transparent linear-gradient(119deg, #009efd 0%, #2af597 100%) 0%
+    0% no-repeat padding-box;
+}
+.green {
+  background: transparent linear-gradient(119deg, #56ab2f 0%, #a8e063 100%) 0%
+    0% no-repeat padding-box;
+}
+.brown {
+  background: transparent linear-gradient(300deg, #f5515f 0%, #a1051d 100%) 0%
+    0% no-repeat padding-box;
+}
+.color-cards {
+  box-sizing: border-box;
+  border-radius: 8px;
+  max-width: 24%;
+  min-width: 24%;
+  padding: 1rem;
+  height: 12rem;
+  box-shadow: 0px 5px 9px #0000002b;
+  font-weight: 500;
+}
+.color_heading {
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  font-size: 18px;
+  margin: 0;
+}
+</style>
